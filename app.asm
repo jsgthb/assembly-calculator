3c62520c7f6351665aed13fe01d6b877a1ceb62c
jsgthb <jeff.schaack@gmail.com>
2024-06-23 20:27:42 +0200

    Simple string output

---
+++ app.asm
@@ -0,0 +1,22 @@
+section .bss
+    ; variables
+
+section .data
+    ; constants
+    teststring: db "This is a test",0xa
+    teststringlen: equ $-teststring
+
+section .text
+    global _start
+
+    _start:
+        mov rax,1   ; sys_write
+        mov rdi,1   ; stdout
+        mov rsi,teststring  ; message to write
+        mov rdx,teststringlen  ; message length to write
+        syscall
+
+        ; end program
+        mov rax,60  ; sys_exit
+        mov rdi,0   ; error code 0 (success)
+        syscall
