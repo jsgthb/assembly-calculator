a32de2986e54bd96a69f6a9081a04592cb3dab29
jsgthb <jeff.schaack@gmail.com>
2024-10-29 23:53:11 +0100

    Removed indentation

--- app.asm
+++ app.asm
@@ -7,16 +7,16 @@ section .data
     teststringlen: equ $-teststring
 
 section .text
-    global _start
+global _start
 
-    _start:
-        mov rax,1   ; sys_write
-        mov rdi,1   ; stdout
-        mov rsi,teststring  ; message to write
-        mov rdx,teststringlen  ; message length to write
-        syscall
+_start:
+    mov rax,1   ; sys_write
+    mov rdi,1   ; stdout
+    mov rsi,teststring  ; message to write
+    mov rdx,teststringlen  ; message length to write
+    syscall
 
-        ; end program
-        mov rax,60  ; sys_exit
-        mov rdi,0   ; error code 0 (success)
-        syscall
+    ; end program
+    mov rax,60  ; sys_exit
+    mov rdi,0   ; error code 0 (success)
+    syscall
